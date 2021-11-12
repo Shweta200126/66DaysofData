b7ba524febbe0aa882d21ddbf4dac4a54b2d20ef
Shweta200126 <anand26shweta@gmail.com>
2021-11-12 13:27:22 +0530

    file of exercise solutions added

---
+++ Days/Python/solutions.py
@@ -0,0 +1,38 @@
+#Ask for radius and print out area
+
+# pi = 3.14
+# r = int(input("Enter radius of the circle\n>>>"))
+# area = pi * (r**2)
+# print("\nArea of circle of given radius is = ", area)
+
+
+#Convert Fahrenheit to Celsius
+
+# y = float(input("Enter fahrenheit:"))
+# x = (y - 32)*(5/9)
+# print("Fahrenheit temp is,", y, ", Celsius temp is", x)
+
+
+#Sum of two integers
+
+# x = int(input("Enter an interger:"))
+# y = int(input("Enter another interger:"))
+# sum = x + y
+# print("The sum of integers is:", sum)
+
+
+#Product of two integers
+
+# num_1 = int(input("Enter an integer:"))
+# num_2 = int(input("Enter another integer:"))
+# print("The product of "+str(num_1)+' and '+str(num_2)+' is '+str(num_1*num_2))
+
+
+# Bob, Ann, Jane and Ashwin want to order pizza - they know they will each eat
+# 4 slices of pizza. The local pizza shop sells pizzas of only 6 slices
+# What is the minimum number of pizzas needed - Use floor division
+
+#req_slices = 4*4
+#num_of_pizzas = (req_slices+5)//6
+#slices_needed = num_of_pizzas*6 - req_slices
+#print("The min. pizzas needed are, ", num_of_pizzas, ", slices left are, ", slices_needed)
